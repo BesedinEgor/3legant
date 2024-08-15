98226f8dbb76d0dbbc41f1803aa9a3fcb41ca48a
Egor <Besedinegor@mail.ru>
2024-08-15 22:05:54 +0300

    подключение swiper для блока dproducts

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
 import mobileNav from './modules/mobile-nav.js';
+
+const productSwiper = new Swiper('.products-swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: '.products-swiper-pagination',
+    },
+  });
+
+
 mobileNav();
