91dd9ce9ac15e33cbdbf87248794ded602c68ce9
Egor <Besedinegor@mail.ru>
2024-08-15 19:34:10 +0300

    реализация появления мобильного меню + реализацю меню, исправление мелких багов

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -3,12 +3,19 @@ function mobileNav() {
 	const navBtn = document.querySelector('.mobile-nav-btn');
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
+	const closeMenu = document.querySelector('.mobile-nav__close');
 
 	navBtn.onclick = function () {
 		nav.classList.toggle('mobile-nav--open');
 		menuIcon.classList.toggle('nav-icon--active');
 		document.body.classList.toggle('no-scroll');
 	};
+
+	closeMenu.onclick = function () {
+		nav.classList.remove('mobile-nav--open');
+		menuIcon.classList.remove('nav-icon--active');
+		document.body.classList.remove('no-scroll');
+	}
 }
 
 export default mobileNav;
