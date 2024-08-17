e784fe033dea1ea9b1bdef21dac2edebb0522c93
Egor <Besedinegor@mail.ru>
2024-08-17 21:19:32 +0300

    начат слайдер и реализована карточка товара статичная(шаблон)

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,9 @@ import 'swiper/css/bundle';
 import mobileNav from './modules/mobile-nav.js';
 
 const productSwiper = new Swiper('.products-swiper', {
-    slidesPerView: 1,
-    loop: true,
+    slidesPerView: 1.6,
+    loop: false,
+    spaceBetween: 16,
     pagination: {
       el: '.products-swiper-pagination',
     },
