8363284b3d2ce587fa196058e8d599178c40a525
Egor <Besedinegor@mail.ru>
2024-08-19 12:57:03 +0300

    замена изображений на другой формат в карточках товара, адаптивные изображения, добавлены первые миксины, правки container-right в base

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,11 @@ import "swiper/css/bundle";
 import mobileNav from "./modules/mobile-nav.js";
 
 const productSwiper = new Swiper(".products-swiper", {
-  slidesPerView: 1.6,
+  slidesPerView: "auto",
   loop: false,
   spaceBetween: 16,
-
   pagination: {
     el: ".products-swiper-pagination",
-    dynamicBullets: false,
   },
 });
 
